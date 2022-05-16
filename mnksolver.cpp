4c673cbcf8383cc6248046e9cd6e33dea8eb9757
Toster <krzysna@gmail.com>
2022-05-16 07:30:11 +0200

    poprawione m n

--- mnksolver.cpp
+++ mnksolver.cpp
@@ -7,8 +7,12 @@ int main() {
   int n,m,k;
   Player activePlayer;
   char cmd[33];
-  scanf("%32s %i %i %i %i",cmd,&n,&m,&k,&activePlayer);
-  MNKSolver solver(cmd,n,m,k,activePlayer);
-  solver.print();
+  while(true) {
+    scanf("%32s %i %i %i %i",cmd,&n,&m,&k,&activePlayer);
+    if(feof(stdin)!=0)
+      break;
+    MNKSolver solver(cmd,n,m,k,activePlayer);
+    solver.print();
+  }
   return 0;
 }
